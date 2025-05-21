9b8e787a4dc7455dc70ce0261d5b18a8ed9451f6
Enrique <enrique2002.s.m@gmail.com>
2025-05-21 15:23:36 +0200

    Update main.jsx

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,18 +1,49 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-
-//Bootstrap
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom/client';
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap"
+import "bootstrap";
+import '../styles/index.css';
+import Semaforo from './components/Semaforo';
 
-// index.css'
-import '../styles/index.css'
+function App() {
+  const [color, setColor] = useState("rojo");
 
-// components
-import Home from './components/Home';
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setColor((prevColor) => {
+        if (prevColor === "rojo") return "verde";
+        if (prevColor === "verde") return "amarillo";
+        return "rojo";
+      });
+    }, color === "amarillo" ? 3000 : 10000);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
-)
+    return () => clearInterval(intervalId);
+  }, [color]);
+
+  function elegirColor(nuevoColor) {
+    setColor(nuevoColor);
+  }
+
+  function colorRnd() {
+    const colores = ["rojo", "amarillo", "verde"];
+    const colorAleatorio = colores[Math.floor(Math.random() * colores.length)];
+    setColor(`${colorAleatorio}`);
+  }
+
+  function colorWhite() {
+    const colores = ["rojo", "amarillo", "verde"];
+    const colorAleatorio = colores[Math.floor(Math.random() * colores.length)];
+    setColor(`${colorAleatorio}-blanco`);
+  }
+
+  return (
+    <div>
+      <Semaforo color={color} elegirColor={elegirColor} />
+      <button onClick={colorWhite}>FLASHBANG!</button>
+      <button onClick={colorRnd}>Random</button>
+    </div>
+  );
+}
+
+  // Render
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
